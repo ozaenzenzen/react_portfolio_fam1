bf3efb12e72682671391788a42185128f42ffbd5
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-28 18:35:55 +0700

    add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -24,6 +25,7 @@ function App() {
         <Technologies />
         <Experience />
         <Projects />
+        <Contact />
       </div>
     </div>
   );
