608de20d1687e3af957bdc40ce75291a70f73b97
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-28 20:22:42 +0700

    fix navbar title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,8 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img src={logo} alt="" className="mx-2 w-10"/>
+        {/* <img src={logo} alt="" className="mx-2 w-10"/> */}
+        <h1 className="text-4xl">Fauzan's Lab</h1>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
