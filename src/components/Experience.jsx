d8fe246b15c9fd63c685415b73a5ec4b3a366020
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-28 20:13:36 +0700

    update motion in experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 import { EXPERIENCES } from "../constants";
+import { motion } from "framer-motion";
 
 const Experience = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">Experience</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Experience
+      </motion.h2>
       <div>
         {EXPERIENCES.map((item, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
+            {/* Year Section */}
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/4"
+            >
               <p className="mb-2 text-sm text-neutral-400">{item.year}</p>
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
+            </motion.div>
+
+            {/* Experience Desc Section */}
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
               <h6 className="mb-2 font-semibold">
                 {item.role} -{" "}
                 <span className="text-sm text-purple-100">{item.company}</span>
@@ -25,7 +46,7 @@ const Experience = () => {
                   {itemTech}
                 </span>
               ))}
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
